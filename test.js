8f795ff74c4bb29b8fd8bfe44ac2759a151f4f0d
Hackbright Student <info@hackbrightacademy.com>
2016-04-27 18:01:10 -0700

    Progressed to advanced study part II)

---
+++ test.js
@@ -0,0 +1,26 @@
+word = "cristina"
+newWord = ""
+listOfUsedNumbers = []
+while true
+var myNumber = Math.floor(Math.random() * word.length );
+if myNumber is not in listOfUsedNumbers
+    call index of word
+    push into newWord
+    once newword is equal to word
+    break
+
+
+var word = "cristina";
+var newWord = "";
+var listOfUsedNumbers = [];
+
+while (true) {
+    var i = Math.floor(Math.random() * word.length - 1);
+    var checker = listOfUsedNumbers.indexOf(i);
+        if checker === -1
+            
+}
+
+
+
+
